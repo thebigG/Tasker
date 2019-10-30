207fb059207bae18c1cde5dde4ca7b540f1b6b24
Gemuele Aludino <g.aludino@gmail.com>
2019-10-30 17:45:29 -0400

    Fixed folder structure for repo - all unnecessary files removed.
    documentation
        assignments
        canvas_files
        gui_sketches
        reports
        visual_paradigm
    Tasker
        (all headers/sources/Qt project files)

--- Shape.h
+++
@@ -1,7 +0,0 @@
-class Shape
-{
-public:
-    virtual int get_area() = 0;
-    virtual int get_perimeter() = 0;
-
-};
--- standard_stuff.h
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
-#include<cmath>
-using namespace std;
