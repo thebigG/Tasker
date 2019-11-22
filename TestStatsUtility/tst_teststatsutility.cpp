d93d25d48f8ded36d318378c42062b1db795889f
Gemuele Aludino <gemuelealudino@nbp-226-188.nbp.ruw.rutgers.edu>
2019-11-22 18:07:28 -0500

    Test cases for StatsUtility, and started User

--- TestStatsUtility/tst_teststatsutility.cpp
+++ TestStatsUtility/tst_teststatsutility.cpp
@@ -2,40 +2,164 @@
 
 #include "StatsUtility.h"
 
-class TestStatsUtility : public QObject
-{
+class TestStatsUtility : public QObject {
     Q_OBJECT
 
-public:
-    TestStatsUtility();
-    ~TestStatsUtility();
+    private slots:
+    void testToMinutesPositive();
+    void testToMinutesNegative();
 
-private slots:
-    void test_case1();
-    // void testToMinutes();
-    // void testToHours();
-    // void testToDays();
-    // void testToWeeks();
-    // void testToMonths();
-    // void calculateProductivePercentage();
+    void testToHoursPositive();
+    void testToHoursNegative();
 
+    void testToDaysPositive();
+    void testToDaysNegative();
+
+    void testToWeeksPositive();
+    void testToWeeksNegative();
+
+    void testToMonthsPositive();
+    void testToMonthsNegative();
+
+    void calculateProductivePercentageAllNegative();
+    void calculateProductivePercentageNegativeSecondsPositiveProductive();
+    void calculateProductivePercentagePositiveSecondsNegativeProductive();
+    void calculateProductivePercentageAllPositive();
 };
 
-TestStatsUtility::TestStatsUtility()
-{
-
-}
-
-TestStatsUtility::~TestStatsUtility()
-{
-
-}
-
-void TestStatsUtility::test_case1()
-{
-
-}
-
-QTEST_APPLESS_MAIN(TestStatsUtility)
+QTEST_MAIN(TestStatsUtility)
 
 #include "tst_teststatsutility.moc"
+
+using util::StatsUtility;
+
+/**
+ * @brief TestStatsUtility::testToMinutesPositive
+ *
+ */
+void TestStatsUtility::testToMinutesPositive() {
+    int64_t value = 240;
+
+    QVERIFY(StatsUtility::toMinutes(value) > -1);
+}
+
+/**
+ * @brief TestStatsUtility::testToMinutesNegative
+ */
+void TestStatsUtility::testToMinutesNegative() {
+    int64_t value = -99;
+
+    QVERIFY(StatsUtility::toMinutes(value) == -1);
+}
+
+/**
+ * @brief TestStatsUtility::testToHoursPositive
+ */
+void TestStatsUtility::testToHoursPositive() {
+    int64_t value = 240;
+
+    QVERIFY(StatsUtility::toHours(value) > -1);
+}
+
+/**
+ * @brief TestStatsUtility::testToHoursNegative
+ */
+void TestStatsUtility::testToHoursNegative() {
+    int64_t value = -99;
+
+    QVERIFY(StatsUtility::toHours(value) == -1);
+}
+
+/**
+ * @brief TestStatsUtility::testToDaysPositive
+ */
+void TestStatsUtility::testToDaysPositive() {
+    int64_t value = 240;
+
+    QVERIFY(StatsUtility::toDays(value) > -1);
+}
+
+/**
+ * @brief TestStatsUtility::testToDaysNegative
+ */
+void TestStatsUtility::testToDaysNegative() {
+    int64_t value = -99;
+
+    QVERIFY(StatsUtility::toDays(value) == -1);
+}
+
+/**
+ * @brief TestStatsUtility::testToWeeksPositive
+ */
+void TestStatsUtility::testToWeeksPositive() {
+    int64_t value = 240;
+
+    QVERIFY(StatsUtility::toWeeks(value) > -1);
+}
+
+/**
+ * @brief TestStatsUtility::testToWeeksNegative
+ */
+void TestStatsUtility::testToWeeksNegative() {
+    int64_t value = -99;
+
+    QVERIFY(StatsUtility::toWeeks(value) == -1);
+}
+
+/**
+ * @brief TestStatsUtility::testToMonthsPositive
+ */
+void TestStatsUtility::testToMonthsPositive() {
+    int64_t value = 240;
+
+    QVERIFY(StatsUtility::toMonths(value) > -1);
+}
+
+/**
+ * @brief TestStatsUtility::testToMonthsNegative
+ */
+void TestStatsUtility::testToMonthsNegative() {
+    int64_t value = -99;
+
+    QVERIFY(StatsUtility::toMonths(value) == -1);
+}
+
+/**
+ * @brief TestStatsUtility::calculateProductivePercentageAllNegative
+ */
+void TestStatsUtility::calculateProductivePercentageAllNegative() {
+    int64_t valueSeconds = -99;
+    int64_t valueProductive = -66;
+
+    QVERIFY(StatsUtility::calculateProductivePercentage(valueSeconds, valueProductive) == -1.0);
+}
+
+/**
+ * @brief TestStatsUtility::calculateProductivePercentageNegativeSecondsPositiveProductive
+ */
+void TestStatsUtility::calculateProductivePercentageNegativeSecondsPositiveProductive() {
+    int64_t valueSeconds = -99;
+    int64_t valueProductive = 66;
+
+    QVERIFY(StatsUtility::calculateProductivePercentage(valueSeconds, valueProductive) == -1.0);
+}
+
+/**
+ * @brief TestStatsUtility::calculateProductivePercentagePositiveSecondsNegativeProductive
+ */
+void TestStatsUtility::calculateProductivePercentagePositiveSecondsNegativeProductive() {
+    int64_t valueSeconds = 99;
+    int64_t valueProductive = -66;
+
+    QVERIFY(StatsUtility::calculateProductivePercentage(valueSeconds, valueProductive) == -1.0);
+}
+
+/**
+ * @brief TestStatsUtility::calculateProductivePercentageAllPositive
+ */
+void TestStatsUtility::calculateProductivePercentageAllPositive() {
+    int64_t valueSeconds = 99;
+    int64_t valueProductive = 66;
+
+    QVERIFY(StatsUtility::calculateProductivePercentage(valueSeconds, valueProductive) > -1.0);
+}
--- build-TestStatsUtility-Desktop_Qt_5_12_5_clang_64bit-Debug/target_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DYLD_IMAGE_SUFFIX=_debug
-export DYLD_IMAGE_SUFFIX
-DYLD_FRAMEWORK_PATH=/Users/gemuelealudino/Qt/5.12.5/clang_64/lib${DYLD_FRAMEWORK_PATH:+:$DYLD_FRAMEWORK_PATH}
-export DYLD_FRAMEWORK_PATH
-QT_PLUGIN_PATH=/Users/gemuelealudino/Qt/5.12.5/clang_64/plugins${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}
-export QT_PLUGIN_PATH
-exec "$@"
