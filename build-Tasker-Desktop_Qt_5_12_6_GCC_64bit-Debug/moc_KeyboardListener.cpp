c206eaa10c4368b5d1b3c2edd01244cd4851545f
Lorenzo Gomez <yunior.eury@gmail.com>
2019-12-04 18:49:48 -0500

    Fixing heads

--- build-Tasker-Desktop_Qt_5_12_6_GCC_64bit-Debug/moc_KeyboardListener.cpp
+++ build-Tasker-Desktop_Qt_5_12_6_GCC_64bit-Debug/moc_KeyboardListener.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Engine__KeyboardListener_t {
-    QByteArrayData data[8];
-    char stringdata0[66];
+    QByteArrayData data[7];
+    char stringdata0[62];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,12 +37,11 @@ QT_MOC_LITERAL(2, 38, 0), // ""
 QT_MOC_LITERAL(3, 39, 5), // "start"
 QT_MOC_LITERAL(4, 45, 3), // "end"
 QT_MOC_LITERAL(5, 49, 5), // "pause"
-QT_MOC_LITERAL(6, 55, 3), // "run"
-QT_MOC_LITERAL(7, 59, 6) // "update"
+QT_MOC_LITERAL(6, 55, 6) // "update"
 
     },
     "Engine::KeyboardListener\0signalThread\0"
-    "\0start\0end\0pause\0run\0update"
+    "\0start\0end\0pause\0update"
 };
 #undef QT_MOC_LITERAL
 
@@ -52,7 +51,7 @@ static const uint qt_meta_data_Engine__KeyboardListener[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,20 +59,18 @@ static const uint qt_meta_data_Engine__KeyboardListener[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,   44,    2, 0x06 /* Public */,
+       1,    0,   39,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       3,    0,   45,    2, 0x0a /* Public */,
-       4,    0,   46,    2, 0x0a /* Public */,
-       5,    0,   47,    2, 0x0a /* Public */,
-       6,    0,   48,    2, 0x0a /* Public */,
-       7,    0,   49,    2, 0x0a /* Public */,
+       3,    0,   40,    2, 0x0a /* Public */,
+       4,    0,   41,    2, 0x0a /* Public */,
+       5,    0,   42,    2, 0x0a /* Public */,
+       6,    0,   43,    2, 0x0a /* Public */,
 
  // signals: parameters
     QMetaType::Void,
 
  // slots: parameters
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -92,8 +89,7 @@ void Engine::KeyboardListener::qt_static_metacall(QObject *_o, QMetaObject::Call
         case 1: _t->start(); break;
         case 2: _t->end(); break;
         case 3: _t->pause(); break;
-        case 4: _t->run(); break;
-        case 5: _t->update(); break;
+        case 4: _t->update(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -138,13 +134,13 @@ int Engine::KeyboardListener::qt_metacall(QMetaObject::Call _c, int _id, void **
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 5;
     }
     return _id;
 }
