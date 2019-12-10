3e6daae391218f34b52497f8c82ef265d8ac818f
Lorenzo Gomez <yunior.eury@gmail.com>
2019-12-10 18:38:19 -0500

    Timer works now with both listsners

--- build-Tasker-Desktop_Qt_5_12_6_GCC_64bit-Debug/moc_Timer.cpp
+++ build-Tasker-Desktop_Qt_5_12_6_GCC_64bit-Debug/moc_Timer.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Engine__Timer_t {
-    QByteArrayData data[4];
-    char stringdata0[38];
+    QByteArrayData data[5];
+    char stringdata0[59];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,10 +34,12 @@ static const qt_meta_stringdata_Engine__Timer_t qt_meta_stringdata_Engine__Timer
 QT_MOC_LITERAL(0, 0, 13), // "Engine::Timer"
 QT_MOC_LITERAL(1, 14, 13), // "startListener"
 QT_MOC_LITERAL(2, 28, 0), // ""
-QT_MOC_LITERAL(3, 29, 8) // "timeSlot"
+QT_MOC_LITERAL(3, 29, 8), // "timeSlot"
+QT_MOC_LITERAL(4, 38, 20) // "startBackgroundTimer"
 
     },
-    "Engine::Timer\0startListener\0\0timeSlot"
+    "Engine::Timer\0startListener\0\0timeSlot\0"
+    "startBackgroundTimer"
 };
 #undef QT_MOC_LITERAL
 
@@ -47,7 +49,7 @@ static const uint qt_meta_data_Engine__Timer[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -55,15 +57,17 @@ static const uint qt_meta_data_Engine__Timer[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x06 /* Public */,
+       1,    0,   29,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       3,    0,   25,    2, 0x0a /* Public */,
+       3,    0,   30,    2, 0x0a /* Public */,
+       4,    0,   31,    2, 0x0a /* Public */,
 
  // signals: parameters
     QMetaType::Void,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -77,6 +81,7 @@ void Engine::Timer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
         switch (_id) {
         case 0: _t->startListener(); break;
         case 1: _t->timeSlot(); break;
+        case 2: _t->startBackgroundTimer(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -121,13 +126,13 @@ int Engine::Timer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
