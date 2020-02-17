f25a41b60f9a76fb693d468ccacb17f4e9c536c4
Lorenzo Gomez <yunior.eury@gmail.com>
2020-02-16 21:49:47 -0500

    keyboard AND mouse listener support setup
    We will be using iohook for our our keyboard AND mouse listeners. iohook is cross-platform and has an awesome community of maintainers. Even though
    iohook is a nodejs extension, it is written enturely in C! Which means we will be integrating it natively with Tasker.

---
+++ dependencies/iohook_compilation_flas.sh
@@ -0,0 +1,2 @@
+ gcc -no-pie  ~/playground/JS/libuiohook/src/demo_hook.c /usr/local/lib/libuiohook.a -lXt  -lX11 -lxcb /usr/lib/x86_64-linux-gnu/libX*.a /usr/lib/x86_64-linux-gnu/libxkb*.a /usr/lib/x86_64-linux-gnu/libxcb*.a /usr/lib/x86_64-linux-gnu/libXext.a
+
