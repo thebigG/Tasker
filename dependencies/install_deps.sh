fee9d1bd4908af02adc833c171145d19a607f532
thebigg <yunior.eury@gmail.com>
2021-02-19 21:41:37 -0600

    -Added dependencies script

---
+++ dependencies/install_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Install dependencies for libuiohook
+sudo apt-get install libx11-dev
+sudo apt-get install libxtst-dev
+sudo apt-get install libxt-dev
+sudo apt-get install libxinerama-dev
+sudo apt-get install libx11-xcb-dev
+sudo apt-get install libxkbcommon-dev
+sudo apt-get install libxkbcommon-x11-dev
+sudo apt-get install libxkbfile-dev
+
+
+#TODO: Install dependencies for jackd libraries and jack plugin
+
